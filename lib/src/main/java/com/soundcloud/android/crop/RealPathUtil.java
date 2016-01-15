30bc5796a22709d798c50d449e59027301c1dfc9
TheWhiteLlama <das.weisse.lama@gmail.com>
2016-01-16 00:43:04 +0100

    - testing with RealPathUtil

--- lib/src/main/java/com/soundcloud/android/crop/RealPathUtil.java
+++ lib/src/main/java/com/soundcloud/android/crop/RealPathUtil.java
@@ -1,88 +1,26 @@
 package com.soundcloud.android.crop;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.CursorLoader;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 
 public class RealPathUtil {
 
     public static String getRealPath(Context context, Uri uri) {
 
-        if (Build.VERSION.SDK_INT < 11) {
-            // SDK < API11
-            return RealPathUtil.getRealPathFromURI_BelowAPI11(context, uri);
-        } else if (Build.VERSION.SDK_INT < 19) {
-            // SDK >= 11 && SDK < 19
-            return RealPathUtil.getRealPathFromURI_API11to18(context, uri);
-        } else {
-            // SDK > 19 (Android 4.4)
-            return RealPathUtil.getRealPathFromURI_API19(context, uri);
-        }
-
-    }
-
-    @SuppressLint("NewApi")
-    public static String getRealPathFromURI_API19(Context context, Uri uri) {
-
-        String filePath = "";
-        String wholeID = DocumentsContract.getDocumentId(uri);
-
-        // Split at colon, use second item in the array
-        String id = wholeID.split(":")[1];
-
-        String[] column = {MediaStore.Images.Media.DATA};
-
-        // where id is equal to
-        String sel = MediaStore.Images.Media._ID + "=?";
-
-        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                column, sel, new String[]{id}, null);
-
-        int columnIndex = cursor.getColumnIndex(column[0]);
-
-        if (cursor.moveToFirst()) {
-            filePath = cursor.getString(columnIndex);
-        }
-
-        cursor.close();
-
-        return filePath;
-
-    }
-
-
-    @SuppressLint("NewApi")
-    public static String getRealPathFromURI_API11to18(Context context, Uri contentUri) {
-
-        String[] proj = {MediaStore.Images.Media.DATA};
-        String result = null;
-
-        CursorLoader cursorLoader = new CursorLoader(
-                context,
-                contentUri, proj, null, null, null);
-        Cursor cursor = cursorLoader.loadInBackground();
-
-        if (cursor != null) {
-            int column_index =
-                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(uri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             cursor.moveToFirst();
-            result = cursor.getString(column_index);
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
         }
-
-        return result;
-
     }
 
-    public static String getRealPathFromURI_BelowAPI11(Context context, Uri contentUri) {
-        String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        return cursor.getString(column_index);
-    }
 }
