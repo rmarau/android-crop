db996c1776d783b23ee90afa09518c81f5f81214
TheWhiteLlama <das.weisse.lama@gmail.com>
2016-01-16 00:54:26 +0100

    - testing with RealPathUtil v2

--- lib/src/main/java/com/soundcloud/android/crop/Log.java
+++ lib/src/main/java/com/soundcloud/android/crop/Log.java
@@ -4,8 +4,20 @@ class Log {
 
     private static final String TAG = "android-crop";
 
-    public static void e(String msg) {
-        android.util.Log.e(TAG, msg);
+    public static void d(String msg) {
+        android.util.Log.d(TAG, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        android.util.Log.d(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        android.util.Log.e(tag, msg);
+    }
+
+    public static void e(String tag, String msg, Throwable e) {
+        android.util.Log.e(tag, msg, e);
     }
 
     public static void e(String msg, Throwable e) {
